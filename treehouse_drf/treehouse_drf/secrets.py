ca7ee9e271c4187f6aee0051b7ccfcf613d22176
oakes <oakesonline@gmail.com>
2016-06-27 11:22:56 -0400

    Initial commit

---
+++ treehouse_drf/treehouse_drf/secrets.py
@@ -0,0 +1,12 @@
+SECRET_KEY = '(2^20doido&^^dp21zndf4c+=@tfgb8pd_@kem@59_@+4^4s5o'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'treehouse_drf',
+        'USER': 'Oakes',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
